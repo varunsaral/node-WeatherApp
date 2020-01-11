0dfc1f281dbe812a418963d85ec03c4ed2873dbe
varunsaral <varunsaral47@gmail.com>
2020-01-11 22:51:01 +0530

    Bad character filtered

--- util/geocode.js
+++ util/geocode.js
@@ -4,7 +4,9 @@ const geocode = (address,callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address)+'.json?access_token=pk.eyJ1IjoibWF0dGZpY2tlIiwiYSI6ImNqNnM2YmFoNzAwcTMzM214NTB1NHdwbnoifQ.Or19S7KmYPHW8YjRz82v6g&limit=1'
     
     request({url,json:true}, (error,{body} = {}) => {
-    
+        if(body.message === 'Forbidden'){
+            callback('No Bad Characters!!!',undefined)
+        }
         if(error){
             callback('Unable to connect!!!',undefined)
         }
